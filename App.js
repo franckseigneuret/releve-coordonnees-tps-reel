ae8c5d880b7e974573c692bcdc561ecc6f49cf10
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-02-17 10:37:11 +0100

    relevé de position en temps réel

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 
 
 export default function App() {
+  const [position, setPosition] = useState({});
+
   useEffect(() => {
     async function askPermissions() {
       var { status } = await Location.requestPermissionsAsync();
       if (status === 'granted') {
-        var location = await Location.getCurrentPositionAsync({});
+        // var location = await Location.getCurrentPositionAsync({}); // relève coordonnées 1 fois
+        Location.watchPositionAsync({ distanceInterval: 10 }, // mises à jour tous les 10 mètres
+          (location) => {
+            console.log(location);
+            setPosition({
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            })
+          }
+        );
       }
     }
     askPermissions();
@@ -25,7 +36,9 @@ export default function App() {
           longitudeDelta: 0.0421,
         }}
       >
-        <Marker coordinate={{latitude: 48.858370, longitude: 2.294481}} />
+        <Marker coordinate={{ latitude: 48.858370, longitude: 2.294481 }} />
+        <Marker coordinate={{ latitude: position.latitude, longitude: position.longitude }} title="Hello"
+          description="I am here" />
       </MapView>
       <StatusBar style="auto" />
     </View>
@@ -35,6 +48,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
