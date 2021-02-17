65a71e293629cbfbb2aae3515d0cb261a0c27844
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-02-17 10:23:02 +0100

    init map + ajout marqueur

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
 
 export default function App() {
+  useEffect(() => {
+    async function askPermissions() {
+      var { status } = await Location.requestPermissionsAsync();
+      if (status === 'granted') {
+        var location = await Location.getCurrentPositionAsync({});
+      }
+    }
+    askPermissions();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView style={{ flex: 1, width: '100%' }}
+        initialRegion={{
+          latitude: 48.858370,
+          longitude: 2.294481,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker coordinate={{latitude: 48.858370, longitude: 2.294481}} />
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
